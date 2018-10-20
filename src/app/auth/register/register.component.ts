9faf6d7688d17add6b3b40d78a67483bae05ec86
Emanuel Covaci <covaciemanuel98@gmail.com>
2018-10-21 02:09:48 +0300

    register API works

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {UserService} from '../../services/user.service';
+import {catchError} from 'rxjs/operators';
+import {empty, Observable, EMPTY} from 'rxjs';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+    @Input() public username = null;
+    @Input() public password = null;
+    @Input() public retype_password = null;
+    @Input() public email = null;
+    public error = null;
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    constructor(public userService: UserService) {
+    }
+
+    ngOnInit() {
+    }
+
+    onRegister() {
+        if (this.username && this.password && this.retype_password && this.email) {
+            console.log('go register');
+            this.userService.register(this.username, this.email, this.password).pipe(catchError((error, obs) => {
+                console.log(error);
+                if (!error) {
+                    return obs;
+                } else {
+                    this.error = error;
+                    return EMPTY;
+                }
+            }))
+                .subscribe((user) => {
+                    this.error = undefined;
+                    console.log('registrat');
+                });
+        }
+
+    }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,56 +1,73 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
-import { getCookie, setCookie } from './request-utils';
-import { RequestUtilsService } from './request-utils.service';
-import { flatMap, map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {getCookie, setCookie} from './request-utils';
+import {RequestUtilsService} from './request-utils.service';
+import {flatMap, map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 export interface User {
-  url: string;
-  id: number;
-  username: string;
-  email: string;
+    url: string;
+    id: number;
+    username: string;
+    email: string;
 }
 
 
 @Injectable({
-              providedIn: 'root'
-            })
+    providedIn: 'root'
+})
 export class UserService {
-  apiUrl = environment.apiUrl;
-  user: User;
-  awaitUser: Observable<void>;
+    apiUrl = environment.apiUrl;
+    user: User;
+    awaitUser: Observable<void>;
 
-  constructor(public http: HttpClient,
-              public requestUtils: RequestUtilsService) {
-    if (requestUtils.token) {
-      this.awaitUser = this.getUserByUsername(getCookie('username')).pipe(map(user => {
-        this.user = user;
-        this.awaitUser = null;
-      }));
+    constructor(public http: HttpClient,
+                public requestUtils: RequestUtilsService) {
+        if (requestUtils.token) {
+            this.awaitUser = this.getUserByUsername(getCookie('username')).pipe(map(user => {
+                this.user = user;
+                this.awaitUser = null;
+            }));
+        }
     }
-  }
 
-  login(username: string, password: string) {
-    console.log(this.apiUrl + '/api-token-auth/');
-    return this.http.post(this.apiUrl + '/api-token-auth/', {
-      username, password
-    }, this.requestUtils.requestOptions()).pipe(flatMap((response: { token: string }) => {
-      setCookie('token', response.token);
-      this.requestUtils.token = response.token;
-      setCookie('username', username);
-      return this.getUserByUsername(username).pipe(map(user => {
-        this.user = user;
-        return user;
-      }));
-    }));
-  }
+    login(username: string, password: string) {
+        console.log(this.apiUrl + '/api-token-auth/');
+        return this.http.post(this.apiUrl + '/api-token-auth/', {
+            username, password
+        }, this.requestUtils.requestOptions()).pipe(flatMap((response: { token: string }) => {
+            setCookie('token', response.token);
+            this.requestUtils.token = response.token;
+            setCookie('username', username);
+            return this.getUserByUsername(username).pipe(map(user => {
+                this.user = user;
+                return user;
+            }));
+        }));
+    }
 
-  getUserByUsername(username: string) {
-    return this.http.get(this.apiUrl + `/api/user/?username=${username}`, this.requestUtils.tokenRequestOptions())
-               .pipe(map((users: Array<User>) => {
-                 return users[0];
-               }));
-  }
+    register(username: string, email: string, password: string) {
+        console.log(this.apiUrl + '/api/register/');
+        return this.http.post(this.apiUrl + '/api/register/', {
+            'username': username,
+            'password': password,
+            'email': email
+        }, this.requestUtils.requestOptions()).pipe(map((data: {
+            user: User,
+            token: string
+        }) => {
+            this.user = data.user;
+            setCookie('username', this.user.username);
+            this.requestUtils.token = data.token;
+            setCookie('token', this.requestUtils.token);
+        }));
+    }
+
+    getUserByUsername(username: string) {
+        return this.http.get(this.apiUrl + `/api/user/?username=${username}`, this.requestUtils.tokenRequestOptions())
+            .pipe(map((users: Array<User>) => {
+                return users[0];
+            }));
+    }
 }
