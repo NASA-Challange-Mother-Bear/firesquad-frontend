e2b857017624dbe7e7700d6b75e7763208b2d170
Garovat Adrian <garovat.adrian@gmail.com>
2018-10-21 10:03:29 +0300

    Fixed image post

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,42 +1,39 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {UserService} from '../../services/user.service';
-import {catchError} from 'rxjs/operators';
-import {empty, Observable, EMPTY} from 'rxjs';
+import { Component, Input, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { catchError } from 'rxjs/operators';
+import { empty, Observable, EMPTY } from 'rxjs';
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss']
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
 })
 
 export class LoginComponent implements OnInit {
-    @Input() public username = null;
-    @Input() public password = null;
-    error: string;
+  @Input() public username = null;
+  @Input() public password = null;
+  error: string;
 
-    constructor(
-        public userService: UserService,
-        public router: Router) {
-    }
+  constructor(
+    public userService: UserService,
+    public router: Router) {
+  }
 
-    ngOnInit() {
-    }
+  ngOnInit() {
+  }
 
-    loginForm() {
-        this.userService.login(this.username, this.password)
-            .pipe(catchError((error, obs) => {
-                console.log(error);
-                if (!error) {
-                    return obs;
-                } else {
-                    this.error = error;
-                    return EMPTY;
-                }
-            }))
-            .subscribe((user) => {
-                this.router.navigate(['home']);
-            });
-    }
+  loginForm() {
+    this.userService.login(this.username, this.password)
+      .then((user) => {
+        console.log(user);
+        this.router.navigate(['home']);
+      })
+      .catch((error) => {
+        console.log(error);
+        this.error = error;
+      });
+    ;
+  }
 
 }
